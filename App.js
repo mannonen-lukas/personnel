93a5446049774300783a7366a56ea29449ad3b9d
Mannonen Lukas <lukas.mannonen@hotmail.fr>
2022-05-13 12:00:57 +0200

    exercice1

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import FieldButton from './components/FieldButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FieldButton style={styles.btn} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: 'red',
+    alignItems: 'center',
   },
 });
